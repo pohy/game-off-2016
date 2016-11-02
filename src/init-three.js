a62ba5ed007e604e7229ffd0c3bde1135ce6b8f1
David Pohan <0ronon0@gmail.com>
2016-11-02 15:02:41 +0100

    Add basic shading

--- src/init-three.js
+++ src/init-three.js
@@ -1,29 +1,35 @@
 import * as THREE from 'three';
 import loadOBJ from './load-obj';
+const OrbitControls = require('three-orbit-controls')(THREE);
+import {Noise} from 'noisejs';
 
 export default function() {
     window.addEventListener('resize', onResize, false);
+
+    const noise = new Noise(Math.random());
 
     const {innerWidth: width, innerHeight: height} = window;
     const aspectRatio = width / height;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
-    camera.position.z = 5;
+    camera.position.z = 15;
 
     const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x3d0ce8);
     renderer.setSize(width, height);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
     document.body.appendChild(renderer.domElement);
 
-    const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const floor = createFloor();
-    scene.add(floor);
+    new OrbitControls(camera);
 
-    const cubes = createCubes();
-    // cubes.forEach((cube) => scene.add(cube));
+    const floor = createFloor();
+    floor.receiveShadow = true;
+    scene.add(floor);
 
     const clock = new THREE.Clock();
 
-    const helmetMaterial = new THREE.MeshBasicMaterial({color: 0x3077c9});
+    const helmetMaterial = new THREE.MeshLambertMaterial({color: 0x3077c9});
     let helmet;
     loadOBJ('/assets/helmet.obj')
         .then((object) => {
@@ -32,40 +38,44 @@ export default function() {
                     child.material = helmetMaterial;
                 }
             });
-            object.position.z = -5;
             const scale = 0.1;
             object.scale.x = scale;
             object.scale.y = scale;
             object.scale.z = scale;
             helmet = object;
+            helmet.receiveShadow = true;
+            helmet.castShadow = true;
             scene.add(object)
         })
         .catch(console.error);
+
+    const ambientLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambientLight);
+
+    const spotLight = new THREE.SpotLight(0xffffff, 1, 0, Math.PI / 2);
+    spotLight.position.set(0, 1500, 1000);
+    spotLight.target.position.set(0, 0, 0);
+    spotLight.castShadow = true;
+    spotLight.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, 1, 1200, 2500));
+    spotLight.shadow.bias = 0.0001;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.shadow.mapSize.height = 2048;
+    scene.add(spotLight);
 
     render();
     function render() {
         requestAnimationFrame(render);
 
-        animateCubes(cubes, clock);
-        helmet.rotation.y += Math.sin(clock.getElapsedTime()) * Math.random() * 0.1;
+        if (helmet) {
+            helmet.rotation.y += Math.sin(clock.getElapsedTime()) * Math.random() * 0.1;
+        }
+        floor.material.color.setHSL(
+            (Math.sin(clock.getElapsedTime() / 10) + 1) / 2,
+            1,
+            0.5
+        );
 
         renderer.render(scene, camera);
-    }
-
-    function animateCubes(cubes, clock) {
-        cubes.forEach((cube, i) => {
-            cube.rotation.z += Math.sin(clock.getElapsedTime()) * Math.random() * 0.1;
-            cube.rotation.y += Math.cos(clock.getElapsedTime()) * Math.random() * 0.2;
-
-            cube.position.z = Math.sin(clock.getElapsedTime() + i) * 3;
-            cube.position.z = Math.sin(clock.getElapsedTime() + i) * 3;
-
-            cube.material.color.setHSL(
-                (Math.sin(clock.getElapsedTime() + i) + 1) / 2,
-                1,
-                0.5
-            );
-        });
     }
 
     function onResize() {
@@ -77,30 +87,12 @@ export default function() {
 
     function createFloor() {
         const floorGeometry = new THREE.PlaneGeometry(2000, 2000);
-        const floorMaterial = new THREE.MeshBasicMaterial({color: 0xef58eb, side: THREE.DoubleSide});
+        const floorMaterial = new THREE.MeshPhongMaterial({color: 0xef58eb, side: THREE.DoubleSide});
         const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-        floor.rotation.x = 90;
         floor.position.y = -3;
-        floor.position.z = -1000;
+        floor.rotation.x = Math.PI / 2;
+        floor.receiveShadow = true;
+        floor.castShadow = false;
         return floor;
-    }
-
-    function createCubes() {
-        const rows = 9;
-        const levels = 3;
-        const cubeCount = 85;
-        const cubes = Array
-            .apply(null, Array(cubeCount * levels))
-            .map((arr, i) => {
-                const material = new THREE.MeshBasicMaterial({
-                    color: 0x00ff00
-                });
-                const cube = new THREE.Mesh(boxGeometry, material);
-                cube.position.x = (i - cubeCount / 2) * 0.3;
-                cube.position.y = (i % rows - rows / 2) * 1.5;
-                cube.position.z = i % levels;
-                return cube;
-            });
-        return cubes;
     }
 }
