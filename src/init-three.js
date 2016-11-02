69d57f898cb8858b59d9bfefdc0d0144d61a5ac8
David Pohan <0ronon0@gmail.com>
2016-11-02 11:38:29 +0100

    Refactor to functions

--- src/init-three.js
+++ src/init-three.js
@@ -7,37 +7,18 @@ export default function() {
     const aspectRatio = width / height;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+    camera.position.z = 5;
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height);
     document.body.appendChild(renderer.domElement);
 
     const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const floorGeometry = new THREE.PlaneGeometry(2000, 2000);
-    const floorMaterial = new THREE.MeshBasicMaterial({color: 0xef58eb, side: THREE.DoubleSide});
-    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.rotation.x = 90;
-    floor.position.y = -3;
-    floor.position.z = -1000;
+    const floor = createFloor();
     scene.add(floor);
 
-    camera.position.z = 5;
-    const rows = 9;
-    const levels = 3;
-    const cubes = Array
-        .apply(null, Array(85 * levels))
-        .map(() => {
-            const material = new THREE.MeshBasicMaterial({
-                color: 0x00ff00
-            });
-            return new THREE.Mesh(boxGeometry, material);
-        });
-    cubes.forEach((cube, i) => {
-        cube.position.x = (i - cubes.length / 2) * 0.3;
-        cube.position.y = (i % rows - rows / 2) * 1.5;
-        cube.position.z = i % levels;
-        scene.add(cube);
-    });
+    const cubes = createCubes();
+    cubes.forEach((cube) => scene.add(cube));
 
     const clock = new THREE.Clock();
 
@@ -45,6 +26,12 @@ export default function() {
     function render() {
         requestAnimationFrame(render);
 
+        animateCubes(cubes, clock);
+
+        renderer.render(scene, camera);
+    }
+
+    function animateCubes(cubes, clock) {
         cubes.forEach((cube, i) => {
             cube.rotation.z += Math.sin(clock.getElapsedTime()) * Math.random() * 0.1;
             cube.rotation.y += Math.cos(clock.getElapsedTime()) * Math.random() * 0.2;
@@ -58,8 +45,6 @@ export default function() {
                 0.5
             );
         });
-
-        renderer.render(scene, camera);
     }
 
     function onResize() {
@@ -69,7 +54,32 @@ export default function() {
         renderer.setSize(width, height);
     }
 
-    function randomRange(max, min) {
-        return Math.random() * (max - min) + min;
+    function createFloor() {
+        const floorGeometry = new THREE.PlaneGeometry(2000, 2000);
+        const floorMaterial = new THREE.MeshBasicMaterial({color: 0xef58eb, side: THREE.DoubleSide});
+        const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+        floor.rotation.x = 90;
+        floor.position.y = -3;
+        floor.position.z = -1000;
+        return floor;
+    }
+
+    function createCubes() {
+        const rows = 9;
+        const levels = 3;
+        const cubeCount = 85;
+        const cubes = Array
+            .apply(null, Array(cubeCount * levels))
+            .map((arr, i) => {
+                const material = new THREE.MeshBasicMaterial({
+                    color: 0x00ff00
+                });
+                const cube = new THREE.Mesh(boxGeometry, material);
+                cube.position.x = (i - cubeCount / 2) * 0.3;
+                cube.position.y = (i % rows - rows / 2) * 1.5;
+                cube.position.z = i % levels;
+                return cube;
+            });
+        return cubes;
     }
 }
